18ab7bb211cb04069854073ca91bb88ed5246313
Theodore Dubois <tblodt@icloud.com>
2018-11-06 15:38:48 -0800

    Initial commit

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+version=$(curl -sS https://update.tabnine.com/version)
+case $(uname -s) in
+    "Darwin")
+        platform="apple-darwin"
+        ;;
+    "Linux")
+        platform="unknown-linux-gnu"
+        ;;
+esac
+triple="$(uname -m)-$platform"
+
+cd $(dirname $0)
+path=$version/$triple/TabNine
+if [ -f bin/$path ]; then
+    exit
+fi
+echo Downloading version $version
+curl https://update.tabnine.com/$path --create-dirs -o bin/$path
+chmod +x bin/$path
---
+++ rplugin/python3/deoplete/sources/tabnine.py
@@ -0,0 +1,112 @@
+import json
+import os
+import platform
+import subprocess
+from .base import Base
+from ..util import getlines
+
+class Source(Base):
+    def __init__(self, vim):
+        super().__init__(vim)
+        self.name = 'tabnine'
+        self.mark = '[TN]'
+        self.rank = 500
+        self.proc = None
+        self.is_debug_enabled = True
+
+        self._install_dir = os.path.dirname(os.path.dirname(os.path.dirname(
+            os.path.dirname(os.path.dirname(os.path.realpath(__file__))))))
+
+    def gather_candidates(self, context):
+        LINE_LIMIT = 1000
+        _, line, col, _ = context['position']
+        last_line = self.vim.call('line', '$')
+        before_line = max(1, line - LINE_LIMIT)
+        before_lines = getlines(self.vim, before_line, line)
+        before_lines[-1] = before_lines[-1][:col-1]
+        after_line = min(last_line, line + LINE_LIMIT)
+        after_lines = getlines(self.vim, line, after_line)
+        after_lines[0] = after_lines[0][col:]
+        response = self.request(
+            'Autocomplete',
+            filename=context['bufpath'],
+            before='\n'.join(before_lines),
+            after='\n'.join(after_lines),
+            region_includes_beginning=(before_line == 1),
+            region_includes_end=(after_line == last_line),
+            max_num_results=10,
+        )
+        if response is None:
+            return []
+
+        if len(response['promotional_message']):
+            self.print('Promotional message from TabNine: {}'.format(response['promotional_message'].join('\n')))
+        candidates = []
+        for result in response['results']:
+            candidate = {}
+            word = result['result']
+            prefix_to_substitute = result['prefix_to_substitute']
+            candidate['word'] = candidate['abbr'] = word
+            if word.endswith(prefix_to_substitute):
+                candidate['word'] = word[:len(word)-len(prefix_to_substitute)]
+            candidates.append(candidate)
+        self.info(repr(candidates))
+        return candidates
+
+    def request(self, name, **params):
+        req = {
+            'version': '0.6.0',
+            'request': {name: params}
+        }
+        proc = self.get_running_tabnine()
+        if proc is None:
+            return
+        proc.stdin.write(json.dumps(req) + '\n')
+        proc.stdin.flush()
+        return json.loads(proc.stdout.readline())
+
+    def restart(self):
+        self.print('starting TabNine')
+        if self.proc is not None:
+            self.proc.terminate()
+            self.proc = None
+        binary_dir = os.path.join(self._install_dir, 'bin')
+        path = get_tabnine_path(binary_dir)
+        if path is None:
+            self.print_error('no TabNine binary found')
+            return
+        self.proc = subprocess.Popen(
+            [path, '--client', 'sublime', '--log-file-path', os.path.join(self._install_dir, 'tabnine.log')],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            encoding='utf8',
+        )
+
+    def get_running_tabnine(self):
+        if self.proc is None:
+            self.restart()
+        if self.proc is not None and self.proc.poll():
+            self.print_error('TabNine exited with code {}'.format(self.proc.returncode))
+            self.restart()
+        return self.proc
+
+# Adapted from the sublime plugin
+def parse_semver(s):
+    try:
+        return [int(x) for x in s.split('.')]
+    except ValueError:
+        return []
+
+def get_tabnine_path(binary_dir):
+    SYSTEM_MAPPING = {
+        'Darwin': 'apple-darwin',
+        'Linux': 'unknown-linux-gnu',
+    }
+    versions = os.listdir(binary_dir)
+    versions.sort(key=parse_semver, reverse=True)
+    for version in versions:
+        triple = '{}-{}'.format(platform.machine(), SYSTEM_MAPPING[platform.system()])
+        path = os.path.join(binary_dir, version, triple, 'TabNine')
+        if os.path.isfile(path):
+            return path
