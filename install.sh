ac0c0917a07473ce362a16718f5b3ae66ba20558
superDuperCyberTechno <superDuperCyberTechno@users.noreply.github.com>
2019-02-19 13:21:07 +0100

    rearrange, comments, more descriptive variable names

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
-version=$(curl -sS https://update.tabnine.com/version)
+#
+# Download the latest version of the TabNine binary for UNIX-like operating systems
+
+# Determine client OS
 case $(uname -s) in
     "Darwin")
         platform="apple-darwin"
@@ -10,11 +13,24 @@ case $(uname -s) in
 esac
 triple="$(uname -m)-$platform"
 
+# Go to folder where this script lives
 cd $(dirname $0)
-path=$version/$triple/TabNine
-if [ -f binaries/$path ]; then
-    exit
+
+# Get latest binary version number from TabNine server
+latest_binary_version=$(curl -sS https://update.tabnine.com/version)
+
+# Declare latest binary path
+binary_path=$latest_binary_version/$triple/TabNine
+
+# Check if newest binary exist locally, if it doesn't then download it
+if [ ! -f binaries/$binary_path ]; then
+
+    # Inform user that we're now downloading the latest version
+    echo "Downloading TabNine $latest_binary_version..."
+
+    # Download latest binary
+    curl -sS https://update.tabnine.com/$binary_path --create-dirs -o binaries/$binary_path
+
+    # Set executable permissions on binary
+    chmod +x binaries/$binary_path
 fi
-echo Downloading version $version
-curl https://update.tabnine.com/$path --create-dirs -o binaries/$path
-chmod +x binaries/$path
